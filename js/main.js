320882a3fe3c4725078405c9ad3f34bfc79014de
Julio Aenlle <gmg8r251@ufl.edu>
2016-12-29 11:41:37 -0500

    Try adding slicknav responsive menu

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $(function () {
+        $('#menu').slicknav({
+            label: '',
+            duration: 600,
+            prependTo: '#container',
+        });
+
+        /* $('.slicknav_menu').prepend('<img class="responsive-logo" src="images/iwf-logo.png" title="IWF Construction & Glazing" alt="South Florida construction and glazing services" /></a>');*/
+    });
+
+
+});
